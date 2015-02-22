54ceac30311d0d2ef841c0d8cc352667e0b05088
Deepak <dp@deepakprakash.com>
2015-02-22 12:03:16 +0100

    Add basic `cli` configuration and options.
    Added:
    - Name
    - Usage
    - Version
    
    - `port` flag with 4000 as default.

--- main.go
+++ main.go
@@ -2,9 +2,29 @@ package main
 
 import (
   "os"
+  "fmt"
+
   "github.com/codegangsta/cli"
 )
 
 func main() {
-  cli.NewApp().Run(os.Args)
+  app := cli.NewApp()
+
+  app.Name = "Chattic"
+  app.Usage = "Standalone, self-hosted instant messaging."
+  app.Version = "0.1exp"
+
+  app.Flags = []cli.Flag {
+    cli.IntFlag{
+      Name: "port, p",
+      Value: 4000,
+      Usage: "Port to be used for the server.",
+    },
+  }
+
+  app.Action = func(c *cli.Context) {
+    fmt.Println("Using port:", c.Int("port"))
+  }
+
+  app.Run(os.Args)
 }
