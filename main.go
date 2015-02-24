0501d66920f08ae9c750bd9713ab6fe3399f7c52
Deepak <dp@deepakprakash.com>
2015-02-24 22:46:58 +0100

    Format code with go fmt

--- main.go
+++ main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-  "os"
-  "fmt"
+	"fmt"
+	"os"
 
-  "github.com/codegangsta/cli"
+	"github.com/codegangsta/cli"
 )
 
 func main() {
-  app := cli.NewApp()
+	app := cli.NewApp()
 
-  app.Name = "Chattic"
-  app.Usage = "Standalone, self-hosted instant messaging."
-  app.Version = "0.1exp"
+	app.Name = "Chattic"
+	app.Usage = "Standalone, self-hosted instant messaging."
+	app.Version = "0.1exp"
 
-  app.Flags = []cli.Flag {
-    cli.IntFlag{
-      Name: "port, p",
-      Value: 4000,
-      Usage: "Port to be used for the server.",
-    },
-  }
+	app.Flags = []cli.Flag{
+		cli.IntFlag{
+			Name:  "port, p",
+			Value: 4000,
+			Usage: "Port to be used for the server.",
+		},
+	}
 
-  app.Action = func(c *cli.Context) {
-    fmt.Println("Using port:", c.Int("port"))
-  }
+	app.Action = func(c *cli.Context) {
+		fmt.Println("Using port:", c.Int("port"))
+	}
 
-  app.Run(os.Args)
+	app.Run(os.Args)
 }
